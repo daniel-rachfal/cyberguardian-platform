4f384f344d0e028f3776d82ce973a63e9f3bb3dd
jack wilde <w20022384@northumbria.ac.uk>
2023-03-01 00:12:49 +0000

    Update HomePage.js
    removed previous nav links from home page

--- src/components/pages/HomePage.js
+++ src/components/pages/HomePage.js
@@ -13,12 +13,6 @@ function HomePage() {
                 <p>This is a placeholder page for the Cyberguardian Platform</p>
                 <p>Feel free to yeet all this code (including author) when we get around to this</p>
             </div>
-            <div>
-                <ul>
-                    <li><Link to="/admin">Admin</Link></li>
-                    <li><Link to="/upload">Upload</Link></li>
-                </ul>
-            </div>
         </div>
     );
 }
