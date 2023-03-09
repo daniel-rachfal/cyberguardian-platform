966730cde4edf5e7e4f431d6bc207897c0318e0b
jack wilde <w20022384@northumbria.ac.uk>
2023-03-09 15:44:18 +0000

    implementing htaccess

--- backend/API/index.php
+++ backend/API/index.php
@@ -5,10 +5,23 @@
  * @author Jack Wilde w20022384
  */
 include 'uploadDB.php';
+include 'clienterrorexception.php';
 
 header("Content-Type: application/json; charset=UTF-8");
+header("Access-Control-Allow-Origin: *");
 
- $uploadDB = new uploadDB;
- $output = $uploadDB->getData();
- 
+$url = $_SERVER['REQUEST_URI'];
+$path = parse_url($url)['path'];
+
+switch($path)
+{
+    case '/yr3/cyberguardian-platform/backend/API/upload':
+    case '/yr3/cyberguardian-platform/backend/API/upload/':
+        $uploadDB = new uploadDB;
+        $output = $uploadDB->getData();
+        break;
+    default:
+        throw new ClientErrorException("Path not found");
+}
+
 echo json_encode($output);
