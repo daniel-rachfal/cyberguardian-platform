a116b3518467e142e3c3d63c1e5d7f4c1ee58520
jack wilde <w20022384@northumbria.ac.uk>
2023-03-09 11:15:16 +0000

    uploadDB reformat

--- backend/API/uploadDB.php
+++ backend/API/uploadDB.php
@@ -1,13 +1,25 @@
 <?php 
 include 'database.php';
 
-$db = new Database("DB/development.sqlite");
+Class UploadDB
+{
+    public function __construct()
+    {
+        $this->upload();
+    }
 
-date_default_timezone_set('Europe/London');
-//get current time, unix format
-$date = time();
+    private function upload()
+    {
+        $db = new Database("DB/development.sqlite");
 
-$queryResult = $db->executeSQL("INSERT INTO files 
-                              (fileName, visibility, createdBy, createdAt) 
-                              VALUES ('testFileName', 1, 1, ".$date.")");
-print_r($queryResult);
+        date_default_timezone_set('Europe/London');
+        //get current time, unix format
+        $date = time();
+
+        $queryResult = $db->executeSQL("INSERT INTO files 
+                                    (fileName, visibility, createdBy, createdAt) 
+                                    VALUES ('testFileName', 1, 1, ".$date.")");
+        print_r($queryResult);
+    }
+}
+
