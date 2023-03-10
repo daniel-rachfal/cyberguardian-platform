5eb757c45752699e1beb026d6c96f23c689cd5ac
Daniel Rachfal <rachfaldaniel@gmail.com>
2023-03-10 17:56:40 +0000

    Updated Files endpoint to retrieve visibility
    Changed the naming of retrieved fields to make it more clear

--- backend/API/files.php
+++ backend/API/files.php
@@ -9,8 +9,12 @@ class Files extends Endpoint
 {
     protected function initialiseSQL()
     {
-        $sql = "SELECT files.id, files.fileName, files.visibility, files.createdBy, files.createdAt, users.id, users.email FROM files
-            INNER JOIN users ON files.createdBy = users.id";
+        $sql = "SELECT files.id, files.fileName, files.visibility, files.createdAt, 
+        users.id AS createdById, users.email AS createdByEmail, 
+        file_visibility.name AS visibility 
+            FROM files
+            INNER JOIN users ON files.createdBy = users.id
+            INNER JOIN file_visibility ON files.visibility = file_visibility.id";
         $sqlParams = [];
 
         $this->setSQL($sql);
