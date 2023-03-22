304d20401351c347e39d692eb7b039fb8420c70e
Daniel Rachfal <rachfaldaniel@gmail.com>
2023-03-22 17:17:21 +0000

    Method to return row count from query

--- backend/API/database.php
+++ backend/API/database.php
@@ -5,26 +5,28 @@
  * 
  * @author Nikitas Kaouslidis w20006928
  */
-class Database 
+class Database
 {
     private $dbConnection;
-   
-    public function __construct($dbName) {
+
+    public function __construct($dbName)
+    {
         $this->setDbConnection($dbName);
     }
 
     /**
      * Create database connection using PDO
      */
-    private function setDbConnection($dbName) {
-        try {           
-            $this->dbConnection = new PDO('sqlite:'.$dbName);
+    private function setDbConnection($dbName)
+    {
+        try {
+            $this->dbConnection = new PDO('sqlite:' . $dbName);
             $this->dbConnection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        } catch( PDOException $e ) {
+        } catch (PDOException $e) {
             echo json_encode(["Message: " => $e->getMessage()]);
             exit();
         }
-     }
+    }
 
     /**
      * Execute an SQL prepared statement
@@ -36,19 +38,42 @@ class Database
      * @param  array   $params  An associative array of parameters (default empty array) 
      * @return array            An associative array of the query results
      */
-    public function prepare($sql) {
+    public function prepare($sql)
+    {
         return $this->dbConnection->prepare($sql);
     }
-    public function executeSQL($sql, $params=[]) { 
-        try
-        {
+
+    public function executeSQL($sql, $params = [])
+    {
+        try {
             $stmt = $this->dbConnection->prepare($sql);
             $stmt->execute($params);
 
             return $stmt->fetchAll(PDO::FETCH_ASSOC);
-        } catch(PDOException $e)
-        {
-            echo "Database error! : ".$e->getMessage();
+        } catch (PDOException $e) {
+            echo "Database error! : " . $e->getMessage();
+            exit();
+        }
+    }
+
+    /**
+     * This method executes an SQL statement and returns the number of affected rows. Useful for DELETE and UPDATE operations.
+     * 
+     * @param  string  $sql     An SQL statement
+     * @param  array   $params  An associative array of parameters (default empty array) 
+     * @return int              The number of rows affected
+     * 
+     * @author Daniel Rachfal
+     */
+    public function executeSQLWithRowCount($sql, $params = [])
+    {
+        try {
+            $stmt = $this->dbConnection->prepare($sql);
+            $stmt->execute($params);
+
+            return $stmt->rowCount();
+        } catch (PDOException $e) {
+            echo "Database error! : " . $e->getMessage();
             exit();
         }
     }
