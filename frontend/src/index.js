010a9859cd757d5f1d525eaa3695b077e30b6078
Daniel Rachfal <rachfaldaniel@gmail.com>
2023-03-23 18:47:28 +0000

    Build

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -9,7 +9,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/cyberguardian-platform/frontend">
         <App />
     </BrowserRouter>
   </React.StrictMode>
