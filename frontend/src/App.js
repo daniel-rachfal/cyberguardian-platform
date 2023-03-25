1c094c9feeec269afe11e751d22d2148d4b0e821
daniel-rachfal <rachfaldaniel@gmail.com>
2023-03-25 15:03:55 +0000

    Removed unnecessary admin homepage

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,5 @@
 import './App.css';
 import HomePage from './components/pages/HomePage';
-import AdminHome from './components/pages/admin/AdminHome';
 import ViewAllFiles from './components/pages/admin/ViewAllFiles';
 import UploadPage from './components/pages/UploadPage';
 import SignUp from './components/pages/SignUp';
@@ -15,7 +14,6 @@ function App() {
         <Nav />
         <Routes>
             <Route path="/" element={<HomePage />}/>
-            <Route path="/admin" element={<AdminHome />}/>
             <Route path="/admin/files" element={<ViewAllFiles />}/>
             <Route path="/upload" element={<UploadPage />} />
             <Route path="/preview" element={<PreviewPage />} />
--- frontend/src/components/pages/Nav.js
+++ frontend/src/components/pages/Nav.js
@@ -12,7 +12,7 @@ function Nav() {
         <div>
             <ul>
                 <li><Link to= '/'>Home</Link></li>
-                <li><Link to= '/admin'>Admin</Link></li>
+                <li><Link to= '/admin/files'>Files (Admin)</Link></li>
                 <li><Link to= '/upload'>Upload</Link></li>
                 <li><Link to= '/signup'>Sign Up</Link></li>
                 <li><Link to= '/login'>Login</Link></li>
